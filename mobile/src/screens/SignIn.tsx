b4b5ba1a73adf47e7dba931a3dcb8fc5d600ee4c
Sephyros <leo037br@hotmail.com>
2022-11-06 20:00:18 -0300

    Screens redirection on mobile

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -8,7 +8,7 @@ import { Button } from "../components/Button";
 
 
 export function SignIn() {
-  const { signIn, user } = useAuth()
+  const { signIn, isUserLoading } = useAuth()
   return (
     <Center flex={1} bgColor='gray.900' p={7}>
       <Logo width={212} height={40} />
@@ -16,8 +16,10 @@ export function SignIn() {
         type='SECONDARY'
         title='ENTRAR COM GOOGLE'
         leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
-        mt={4}
+        mt={12}
         onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: 'white' } }}
       />
 
       <Text color='white' textAlign='center' mt={4}>
